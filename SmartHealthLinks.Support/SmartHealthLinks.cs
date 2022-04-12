648a1b4e7f1cda1b3864a4c91384dd58eff05ff9
brian_pos <brian_pos@hotmail.com>
2022-04-12 12:33:26 +1000

    Initial unit testing the Smart Health Links POC project

---
+++ SmartHealthLinks.Support/SmartHealthLinks.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHealthLinks.Support
+{
+    public class SmartLinks
+    {
+        public string access_token { get; set; }
+        public int? expires_in { get; set; }
+        public IEnumerable<SmartResourceAccessRights> authorization_details { get; set; }
+    }
+
+    public class SmartResourceAccessRights
+    {
+        /// <summary>
+        ///  "shlink-view" | "smart-on-fhir-api"
+        /// </summary>
+        public string type { get; set; }
+        public IEnumerable<string> locations { get; set; }
+        public IEnumerable<string> actions { get; set; }
+        public IEnumerable<string> datatypes { get; set; }
+    }
+
+    public class SmartRegistrationRequest
+    {
+        public string token_endpoint_auth_method { get; set; }
+        public IEnumerable<string> grant_types { get; set; }
+        public SmartRegistrationKeys jwks { get; set; }
+        public string client_name { get; set; }
+        public IEnumerable<string> contacts { get; set; }
+    }
+
+    public class SmartRegistrationResponse
+    {
+        public string client_id { get; set; }
+        public string scope { get; set; }
+        public IEnumerable<string> grant_types { get; set; }
+        public SmartRegistrationKeys jwks { get; set; }
+        public string client_name { get; set; }
+        public IEnumerable<string> contacts { get; set; }
+        public string token_endpoint_auth_method { get; set; }
+    }
+
+    public class SmartRegistrationKeys
+    {
+        public IEnumerable<JsonWebKey> keys { get; set; }
+    }
+
+    public class SmartLinksToken
+    {
+        public SmartLinksOAuth oauth { get; set; }
+        public string flags { get; set; }
+        public string decrypt { get; set; }
+        public string prefix { get; set; }
+
+        /// <summary>
+        /// Expiry Epoch seconds
+        /// (to check if the QR is stale)
+        /// </summary>
+        public int? exp { get; set; }
+    }
+
+    public class SmartLinksOAuth
+    {
+        public string url { get; set; }
+        public string token { get; set; }
+    }
+
+    public class VerifiableCredentials
+    {
+        public IEnumerable<string> verifiableCredential { get; set; }
+    }
+}
---
+++ SmartHealthLinks.Test/SmartHealthLinkTests.cs
@@ -0,0 +1,164 @@
+﻿using Hl7.Fhir.Model;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using Task = System.Threading.Tasks.Task;
+using SmartHealthLinks.Support;
+using Hl7.Fhir.Support;
+
+namespace SmartHealthLinks.Test
+{
+    [TestClass]
+    public class SmartHealthLinksTests
+    {
+        [TestMethod]
+        public async Task SmartLinksTester()
+        {
+            // decode Link (reading it from the URL)
+            string smartLinkUrl = "https://demo.vaxx.link/viewer#shlink:/eyJvYXV0aCI6eyJ1cmwiOiJodHRwczovL2FwaS52YXh4LmxpbmsiLCJ0b2tlbiI6Ik1vem1NeVlXaU80ZmJnb21yR0lWdWVlelAzOGZwbGIzS29HMU9hWHMxeTAifSwiZmxhZ3MiOiJQIiwiZGVjcnlwdCI6IlcyOWlhbVZqZENCUFltcGxZM1JkIn0";
+            const string smartLinkName = "#shlink:/";
+            var indexLink = smartLinkUrl.IndexOf(smartLinkName);
+            if (indexLink < 0)
+            {
+                throw new Exception("No Smart Link encoded in the link");
+            }
+            string link = smartLinkUrl.Substring(indexLink + smartLinkName.Length);
+            var smartLinkToken = Base64UrlEncoder.Decode(link);
+            System.Diagnostics.Trace.WriteLine(smartLinkToken);
+
+            // retrieve the content from the provided URL
+            var linkDetails = Newtonsoft.Json.JsonConvert.DeserializeObject<SmartLinksToken>(smartLinkToken);
+
+            HttpClient client = new HttpClient();
+
+            // retrieve the OAuth WellKnown
+            // ${url}/.well-known/smart-configuration
+            string discoveryUrl = $"{linkDetails.oauth.url}/.well-known/smart-configuration";
+            var content = await client.GetAsync(discoveryUrl);
+            string contentJson = await content.Content.ReadAsStringAsync();
+            System.Diagnostics.Trace.WriteLine(contentJson);
+
+            var appLaunchConfiguration = JsonConvert.DeserializeObject<FhirSmartAppLaunchConfiguration>(contentJson);
+            // Check that the app configuration has the required stuff in it
+            Assert.IsTrue(appLaunchConfiguration.capabilities.Contains("shlinks"), "`shlinks` verify this capability is listed");
+            Assert.IsTrue(!string.IsNullOrEmpty(appLaunchConfiguration.registration_endpoint));
+            Assert.IsTrue(!string.IsNullOrEmpty(appLaunchConfiguration.token_endpoint));
+            Assert.IsTrue(appLaunchConfiguration.token_endpoint_auth_methods_supported.Contains("private_key_jwt"), "`private_key_jwt` verify this token_endpoint_auth_methods_supported is listed");
+
+            // Dynamic Client Registration
+            string pin = "1234"; // this is the PIN that was encoded into the QR (provided out of band - likely by the user)
+
+            // Create a new private/public key pair and register them
+            var rsaCrypto = new System.Security.Cryptography.RSACryptoServiceProvider(2048);
+            var rkey = new RsaSecurityKey(rsaCrypto.ExportParameters(false));
+            var k = JsonWebKeyConverter.ConvertFromRSASecurityKey(rkey);
+
+            var regDetails = new SmartRegistrationRequest()
+            {
+                token_endpoint_auth_method = "private_key_jwt",
+                grant_types = new[] { "client_credentials" },
+                jwks = new SmartRegistrationKeys()
+                {
+                    keys = new[] { k } // need to put in our public key here
+                },
+                client_name = "Dr Brian",
+                contacts = new[] { "dr.brian@example.org" }
+            };
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", linkDetails.oauth.token);
+            client.DefaultRequestHeaders.Add("shlink-pin", pin);
+            var jsonFormatter = new JsonMediaTypeFormatter();
+            jsonFormatter.SerializerSettings.DefaultValueHandling = DefaultValueHandling.Include;
+            jsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); // for the JSonWebKey serialization (as default has bad casing)
+            content = await client.PostAsync<SmartRegistrationRequest>(appLaunchConfiguration.registration_endpoint, regDetails, jsonFormatter);
+            contentJson = await content.Content.ReadAsStringAsync();
+            System.Diagnostics.Trace.WriteLine(contentJson);
+
+            var regResponse = JsonConvert.DeserializeObject<SmartRegistrationResponse>(contentJson);
+
+            // Gererate an Authentication JWT and request an access token
+            var payload = new
+            {
+                iss = regResponse.client_id,
+                sub = regResponse.client_id,
+                aud = appLaunchConfiguration.token_endpoint,
+                exp = EpochTime.GetIntDate(DateTime.Now.AddMinutes(5)),
+                nbf = EpochTime.GetIntDate(DateTime.Now),
+                jti = Guid.NewGuid().ToFhirId(),
+            };
+
+            // (Including the extraHeaders here includes the public key in the JWT, and thus can have its signature validated more easily be things like jwt.io
+            //  they are not strictly required, and the OAuth server will be checking the public key that was registered for this client anyway)
+            var extraHeaders = new System.Collections.Generic.Dictionary<string, object>();
+            extraHeaders.Add("typ", "JWT");
+            extraHeaders.Add(JwtHeaderParameterNames.Jwk, k);
+            string assertion = Jose.JWT.Encode(payload, rsaCrypto, Jose.JwsAlgorithm.RS256, extraHeaders);
+            System.Diagnostics.Trace.WriteLine(assertion);
+
+            // Fetch an access token to talk to the server
+            client.DefaultRequestHeaders.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            List<KeyValuePair<string, string>> values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("scope", regResponse.scope));
+            values.Add(new KeyValuePair<string, string>("grant_type", "client_credentials"));
+            values.Add(new KeyValuePair<string, string>("client_assertion_type", "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"));
+            values.Add(new KeyValuePair<string, string>("client_assertion", assertion));
+            FormUrlEncodedContent contentUrl = new FormUrlEncodedContent(values);
+            try
+            {
+                content = await client.PostAsync(appLaunchConfiguration.token_endpoint, contentUrl);
+                contentJson = await content.Content.ReadAsStringAsync();
+                System.Diagnostics.Trace.WriteLine(contentJson);
+
+                // Now read the actual links
+                SmartLinks actualLinks = JsonConvert.DeserializeObject<SmartLinks>(contentJson);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", actualLinks.access_token);
+                foreach (var linkedFile in actualLinks.authorization_details)
+                {
+                    foreach (var locationUrl in linkedFile.locations)
+                    {
+                        System.Diagnostics.Trace.WriteLine("---------------------------------------");
+                        System.Diagnostics.Trace.WriteLine($"{locationUrl}");
+
+                        content = await client.GetAsync(locationUrl);
+                        contentJson = await content.Content.ReadAsStringAsync();
+                        System.Diagnostics.Trace.WriteLine(contentJson);
+                        var vc = JsonConvert.DeserializeObject<VerifiableCredentials>(contentJson);
+                        foreach (var credential in vc?.verifiableCredential)
+                        {
+                            // decode this credential
+                            var parts = credential.Split('.');
+                            JwtHeader header = JwtHeader.Base64UrlDeserialize(parts[0]);
+                            var kid = Base64UrlEncoder.DecodeBytes(header["kid"] as string);
+                            var body = System.Text.UTF8Encoding.UTF8.GetString(new DeflateCompressionProvider().Decompress(Base64UrlEncoder.DecodeBytes(parts[1])));
+                            var jwt = new JwtSecurityToken(header, JwtPayload.Deserialize(body), parts[0], body, parts[1]);
+
+                            // Need to check the validity of the token here
+                            System.Diagnostics.Trace.WriteLine(body);
+                            Newtonsoft.Json.Linq.JObject jwtBody = Newtonsoft.Json.Linq.JObject.Parse(body);
+                            var fhirVersion = jwtBody.SelectToken("vc.credentialSubject.fhirVersion");
+                            var bundleJson = jwtBody.SelectToken("vc.credentialSubject.fhirBundle");
+                            var bundle = new Hl7.Fhir.Serialization.FhirJsonParser().Parse<Bundle>(bundleJson.ToString());
+                            // BasicFacade.DebugDumpOutputXml(bundle);
+
+                            // And resolve the coding display from the content
+                        }
+                    }
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message);
+            }
+        }
+    }
+}
